e0d49a8c5e7ee49006d9aa68b40d12f8d9df742f
j-victor29 <jv4545666@gmail.com>
2026-01-07 23:56:53 -0300

    adiciona novas aplicações Django: users, products, artists, orders e creations

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users',
+    'products',
+    'artists',
+    'orders',
+    'creations',
+    
 ]
 
 MIDDLEWARE = [
