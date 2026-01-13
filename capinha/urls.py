110a84a9e2f71fc19218118b1c0f63b708643a30
Luana Santos <limalu.s92@gmail.comm>
2026-01-13 19:01:55 -0300

    Add redirect from root to API endpoint and update URL routing

--- capinha/urls.py
+++ capinha/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from rest_framework.routers import DefaultRouter
 from capinhacore.views import UsuarioViewSet
 
@@ -8,5 +9,6 @@ router.register(r'usuarios', UsuarioViewSet, basename='usuario')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)), 
+    path('api/', include(router.urls)),
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
     ]
