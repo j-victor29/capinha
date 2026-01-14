89d5a3448e9ae67e167dcdbf325b8d62c3315fb7
maykon-william <maykonvmr@gmail.com>
2026-01-14 01:15:44 -0300

    erro dramatical

--- artists/views.py
+++ artists/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Artista
-from .seralizers import ArtistaSerializer
+from .serializers import ArtistaSerializer
 
 class ArtistaViewSet(ModelViewSet):
     queryset = Artista.objects.filter(ativo=True)
