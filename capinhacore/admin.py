e1056ac01d389264b1c5a7e7d7babf1cc58f58d9
maykon-william <maykonvmr@gmail.com>
2026-01-13 21:44:37 -0300

    Remove pastas desnecessárias e adiciona URLs e serializers em todos os apps.

--- capinhacore/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import TipoUsuario, UsuarioTelefone, UsuarioRecompensa, ContaBancaria, Usuario
-
-admin.site.register(TipoUsuario)
-admin.site.register(UsuarioTelefone)
-admin.site.register(UsuarioRecompensa)
-admin.site.register(ContaBancaria)
-admin.site.register(Usuario)
--- capinhacore/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CapinhacoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'capinhacore'
