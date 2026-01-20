e67dc0888c0ef70fc7a4c6c7d1f5b5a12087d8a8
maykon-william <maykonvmr@gmail.com>
2026-01-19 23:18:29 -0300

    sla

--- artists/views.py
+++ artists/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Artista
